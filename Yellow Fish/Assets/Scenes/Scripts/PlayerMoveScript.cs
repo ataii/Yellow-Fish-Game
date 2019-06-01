00a3f2504edd59dd8fb337f8327787601677ba7b
atajalymkulov <atai.alymkulov@opencbs.com>
2019-06-01 13:57:18 +0600

    Add yellow fish
    - fish can change direction

---
+++ Yellow Fish/Assets/Scenes/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+
+    void Update()
+    {
+        var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
