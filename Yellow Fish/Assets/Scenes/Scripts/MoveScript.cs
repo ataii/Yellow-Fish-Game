5c21eef2dd5dfdc63286b8163cfa350051c5c058
atajalymkulov <atai.alymkulov@opencbs.com>
2019-06-02 11:42:53 +0600

    Add bubble projectiles now yellow fish can attack!

---
+++ Yellow Fish/Assets/Scenes/Scripts/MoveScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        this.transform.Translate(speed * Time.deltaTime, 0f, 0f);
+    }
+}
---
+++ Yellow Fish/Assets/Scenes/Scripts/ShotScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    public Transform bulletPrefab;
+    public float cooldown = 0.25f;
+
+    private float shootCooldown;
+
+    public void Attack()
+    {
+        var shotTransform = Instantiate(bulletPrefab) as Transform;
+        shotTransform.position = transform.position;
+        shotTransform.rotation = transform.rotation;
+    }
+
+    private void Start()
+    {
+        shootCooldown = 0f;
+    }
+
+    private void Update()
+    {
+        if (shootCooldown > 0)
+        {
+            shootCooldown -= Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (CanAttack)
+        {
+            shootCooldown = cooldown;
+        if (Input.GetMouseButton(0))
+        {
+            this.Attack();
+            Debug.Log("attak!");
+        }
+        }
+    }
+
+    public bool CanAttack
+    {
+        get
+        {
+            return shootCooldown <= 0f;
+        }
+    }
+}
