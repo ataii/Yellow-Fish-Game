0d97a29dae291c46013ee5ca393467242b06b4f7
atajalymkulov <atai.alymkulov@opencbs.com>
2019-06-09 14:48:08 +0600

    Add new animation for BlueFish (open mouth)

---
+++ Yellow Fish/Assets/Scripts/BlueFishOpenMouthAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BlueFishOpenMouthAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    public void SetStartValueForAnimation(bool isOpen)
+    {
+        animator.SetBool("isOpen", isOpen);
+    }
+}
